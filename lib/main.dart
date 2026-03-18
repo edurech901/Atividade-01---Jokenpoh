9f29bd181d754377ca017ba7a406a5f96cb873a4
edurech901 <eduardo.rech901@gmail.com>
2026-03-17 21:37:48 -0300

    Finalização da segunda página

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:dev_mobile_flutter/pages/home_page.dart';
+import 'package:dev_mobile_flutter/pages/second_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: HomePage(),
+      home: SecondPage(),
     );
   }
 }
--- lib/pages/second_page.dart
+++ lib/pages/second_page.dart
@@ -1,3 +1,4 @@
+import 'package:dev_mobile_flutter/components/emoji_circle.dart';
 import 'package:flutter/material.dart';
 
 class SecondPage extends StatefulWidget {
@@ -11,12 +12,51 @@ class _SecondPageState extends State<SecondPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GestureDetector(
-        onTap: () {
-          Navigator.pop(context);
-        },
-        child: const Center(
-          child: Text('Segunda Página'),
+      appBar: AppBar(
+        title: const Text("Pedra,Papel, Tesoura"),
+        backgroundColor: Color(0xFFFF1F1F),
+        foregroundColor: Colors.white,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            const Column(
+              children: [
+                EmojiCircle(emoji: '🖐️', buttonSize: 140),
+                SizedBox(height: 16),
+                Text(
+                  "Escolha do App",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            const EmojiCircle(emoji: '👊', buttonSize: 140),
+            Column(
+              children: [
+                Image.asset('assets/vitoria.png', width: 170, height: 170),
+                const SizedBox(height: 16),
+                const Text(
+                  "Você ganhou/perdeu/empatou",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    backgroundColor: const Color(0xFFFF1F1F),
+                    foregroundColor: Colors.white,
+                    padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                  ),
+                  
+                  child: const Text("Tentar Novamente", style: TextStyle(fontSize: 24),),
+                )
+              ],
+            ),
+          ],
         ),
       ),
     );
